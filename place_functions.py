ad1539b2278e34b1c8055fc42753abf338a7c10c
Cody Swain <swain.cody@gmail.com>
2018-07-12 09:32:26 -0700

    __main__

--- place_functions.py
+++ place_functions.py
@@ -3,17 +3,18 @@ from googlemaps import geolocation
 
 from datetime import datetime
 
-# API key shouldn't be stored in code. Move to environment variable. 
-gmaps = googlemaps.Client(key='AIzaSyC94G_D29MnwKOepVNsTOqfu99PYtlcuAU')
+if __name__ == "__main__":
+	# API key shouldn't be stored in code. Move to environment variable. 
+	gmaps = googlemaps.Client(key='AIzaSyC94G_D29MnwKOepVNsTOqfu99PYtlcuAU')
 
-# Geocoding an address 
-geocode_result = gmaps.geocode('161 Camelia Lane, Walnut Creek, Ca')
+	# Geocoding an address 
+	geocode_result = gmaps.geocode('161 Camelia Lane, Walnut Creek, Ca')
 
-# Get directions from one point to another
-now = datetime.now()
-directions_results = gmaps.directions("161 Camelia Lane", "UCLA", mode="transit", departure_time=now)
-print(directions_results)
+	# Get directions from one point to another
+	now = datetime.now()
+	directions_results = gmaps.directions("161 Camelia Lane", "UCLA", mode="transit", departure_time=now)
+	print(directions_results)
 
-# Get current location 
-curr_loc = geolocation.geolocate(client=gmaps)
-print(curr_loc)
+	# Get current location 
+	curr_loc = geolocation.geolocate(client=gmaps)
+	print(curr_loc)
