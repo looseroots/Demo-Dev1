126767e3c9db590a18c9601bf4c3dbc778f34ff6
Cody Swain <swain.cody@gmail.com>
2018-07-12 09:29:29 -0700

    Added directions and current location functions

--- place_functions.py
+++ place_functions.py
@@ -1,4 +1,6 @@
 import googlemaps
+from googlemaps import geolocation
+
 from datetime import datetime
 
 # API key shouldn't be stored in code. Move to environment variable. 
@@ -7,6 +9,11 @@ gmaps = googlemaps.Client(key='AIzaSyC94G_D29MnwKOepVNsTOqfu99PYtlcuAU')
 # Geocoding an address 
 geocode_result = gmaps.geocode('161 Camelia Lane, Walnut Creek, Ca')
 
+# Get directions from one point to another
 now = datetime.now()
 directions_results = gmaps.directions("161 Camelia Lane", "UCLA", mode="transit", departure_time=now)
 print(directions_results)
+
+# Get current location 
+curr_loc = geolocation.geolocate(client=gmaps)
+print(curr_loc)
