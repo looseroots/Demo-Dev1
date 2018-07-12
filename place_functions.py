cc52d6a74801be82f29feff89595e0a69132f4ae
Cody Swain <swain.cody@gmail.com>
2018-07-12 09:09:13 -0700

    Added google api key and simple places query

--- place_functions.py
+++ place_functions.py
@@ -1,1 +1,12 @@
-bingus bongus
+import googlemaps
+from datetime import datetime
+
+# API key shouldn't be stored in code. Move to environment variable. 
+gmaps = googlemaps.Client(key='AIzaSyC94G_D29MnwKOepVNsTOqfu99PYtlcuAU')
+
+# Geocoding an address 
+geocode_result = gmaps.geocode('161 Camelia Lane, Walnut Creek, Ca')
+
+now = datetime.now()
+directions_results = gmaps.directions("161 Camelia Lane", "UCLA", mode="transit", departure_time=now)
+print(directions_results)
