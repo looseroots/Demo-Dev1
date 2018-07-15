07928dfa878b11fc70d8d2c44f7fae13f4cad2fe
Cody Swain <swain.cody@gmail.com>
2018-07-14 21:20:37 -0700

    Created Story class

--- place_functions.py
+++ place_functions.py
@@ -1,11 +1,64 @@
 import googlemaps
 from googlemaps import geolocation
-
+from googlemaps import places
 from datetime import datetime
 
+# Store this in an environment variable
+gmaps = googlemaps.Client(key='AIzaSyC94G_D29MnwKOepVNsTOqfu99PYtlcuAU')
+
+# Stories are lists of events (stored in dicts)
+# Stories should be returned to 
+example_story = [
+	{
+		"Event Title": "Bowling",
+		"Category": "Activity",
+		"Event location": "1234 Haashole Rd, Goober Germany, 91111",
+		"Start Time": "4:00 PM",
+		"End Time": "5:00 PM",
+	},
+	{
+		"Event Title": "Dinner",
+		"Cateogry": "Food",
+		"Event location": "111 Camelia Lane, Walnut Creek, CA 94595",
+		"Start Time": "6:00 PM",
+		"End Time": "7:00 PM",
+	}
+]
+
+class Story():
+	def __init__(self, start_loc=None, end_loc=None, story_duration=None, story_budget=None):
+		self.story = list()
+
+	def start_location(self, location=None):
+		if location:
+			start_loc = gmaps.geocode(location)
+		else:
+			start_loc = geolocation.geolocate(client=gmaps)
+		
+		if start_loc:
+			self.story.append(start_loc)
+
+	# Remove this function. Add class inheritance for "places_nearby"
+	def list_nearby_places(self, location, category):
+		places_dict = places.places_nearby(location, category)
+		return places_dict
+
+	def append_event(self, event):
+		self.story.append(event)
+
+	def choose_category(self, event, time):
+		return None
+
+
+
+# Parameters for Google API place_nearby function
+'''
+def places_nearby(client, location=None, radius=None, keyword=None,
+                  language=None, min_price=None, max_price=None, name=None,
+                  open_now=False, rank_by=None, type=None, page_token=None):
+'''
+
 if __name__ == "__main__":
-	# API key shouldn't be stored in code. Move to environment variable. 
-	gmaps = googlemaps.Client(key='AIzaSyC94G_D29MnwKOepVNsTOqfu99PYtlcuAU')
 
 	# Geocoding an address 
 	geocode_result = gmaps.geocode('161 Camelia Lane, Walnut Creek, Ca')
