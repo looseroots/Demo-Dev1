eb4e8af0dd8fff60fb1400f275112787993329b4
Cody Swain <swain.cody@gmail.com>
2018-07-24 18:19:31 -0700

    Cleaned up functions and commenting

--- place_functions.py
+++ place_functions.py
@@ -1,5 +1,5 @@
 __author__ = "Cody Swain"
-__copyright__ = "TimeSpent" #if your reading this ur a fucking goob
+__copyright__ = "TimeSpent"
 
 import random
 import googlemaps
@@ -7,32 +7,11 @@ from googlemaps import geolocation
 from googlemaps import places
 from datetime import datetime
 
-# ASAP -- Store this in an environment variable for security reasons
+# Migrate to Environment Variable
 gmaps = googlemaps.Client(key='AIzaSyC94G_D29MnwKOepVNsTOqfu99PYtlcuAU')
 
 
-# Stories are lists of events (stored in dicts)
-# Stories should be returned to 
-example_story = [
-	{
-		"Event Title": "Bowling",
-		"Category": "Activity",
-		"Event location": "1234 Haashole Rd, Goober Germany, 91111", # lmao
-		"Start Time": "4:00 PM",
-		"End Time": "5:00 PM",
-	},
-	{
-		"Event Title": "Dinner",
-		"Cateogry": "Food",
-		"Event location": "111 Camelia Lane, Walnut Creek, CA 94595",
-		"Start Time": "6:00 PM",
-		"End Time": "7:00 PM",
-	}
-]
-
-
-# Function called upon creation of a new story. 
-# Returns a users Story as a list a dict inside for event one
+# Create first event
 def initialize_story(start_location=None):
 	story = list() #Individual users story -- A List of Dicts
 
@@ -53,9 +32,7 @@ def initialize_story(start_location=None):
 	return story
 
 
-# Function called upon selection of previous event. May also be called after creation of initial event. 
 # Returns dict with nearby events
-# One issue -- Google Maps API doesn't want developers to scrape large amounts of data. Capped at 60 Search queries. 
 def list_nearby_events(curr_location=None, search_radius=None, event_type=None):
 
 	if event_type:
@@ -68,22 +45,16 @@ def list_nearby_events(curr_location=None, search_radius=None, event_type=None):
 		nearby_events = nearby_gmaps_results['results']
 		return nearby_events
 
+def append_event_to_story(story, event):
+	story.append(event)
+	return story # Python dicts mutable --Remove 'return' ?
 
-# Temp function. List names for object returned by list_nearby_events
+
 def list_nearby_event_names(data):
 	for i in range(len(data)):
 		print(data[i]['name'])
 
-
-# Function called upon the selection of an event. Appends selected event to a users story
-# Returns modified "Story"
-def append_event_to_story(story, event):
-	story.append(event)
-	return story #This return statement may not be necessary because python lists are mutable. Kept for simplicity.
-
-
-# Replace this function in the future
-# Returns randomly selected event from list_nearby_events
+# DEV
 def random_event_picker(nearby_events_list):
 	return random.choice(nearby_events_list)
 
@@ -92,6 +63,24 @@ def random_event_picker(nearby_events_list):
 
 # def allocate_time(self, event):
 # 	return None
+
+# Example structure of story
+example_story = [
+	{
+		"Event Title": "Bowling",
+		"Category": "Activity",
+		"Event location": "1234 Haashole Rd, Goober Germany, 91111", # lmao
+		"Start Time": "4:00 PM",
+		"End Time": "5:00 PM",
+	},
+	{
+		"Event Title": "Dinner",
+		"Cateogry": "Food",
+		"Event location": "111 Camelia Lane, Walnut Creek, CA 94595",
+		"Start Time": "6:00 PM",
+		"End Time": "7:00 PM",
+	}
+]
 
 
 if __name__ == "__main__":
@@ -133,6 +122,16 @@ if __name__ == "__main__":
 
 
 
+
+
+
+
+
+
+
+
+
+
 	### TEMP
 
 	# start_loc = [37.8898303, -122.06701120000002] #This is the starting location 
@@ -147,10 +146,6 @@ if __name__ == "__main__":
 
 
 	### END TEMP
-
-
-
-
 
 	# list_nearby_event_names(places_dict)
 
