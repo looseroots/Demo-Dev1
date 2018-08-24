8870dac34bc5e2a9d1a4be8463624878f3d90db5
Cody Swain <swain.cody@gmail.com>
2018-08-23 21:18:15 -0700

    Added firebase and mobile navbar

--- app/views.py
+++ app/views.py
@@ -1,12 +1,14 @@
+import firebase_admin
+from firebase_admin import db
+
 from flask import render_template, request, session
-from flask import redirect, jsonfiy, redirect
+from flask import redirect, jsonify, redirect
 from app import app
-import random
 
 @app.route('/')
 def home():
 	return render_template('home.html')
 
-@app.route('/get_events')
-def get_events():
-	return jsonify(events=test_events)
+@app.route('/page1')
+def page1():
+	return render_template('page1.html')
